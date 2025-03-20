6e66b26121a2b90c22c6a9639f1d2257e81b8e60
jabjubinski <jabajnldz@gmail.com>
2025-03-20 04:48:53 -0700

    added comment slices and thunk

--- src/redux/slices/commentsSlice.js
+++ src/redux/slices/commentsSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { fetchComments } from "../thunks/commentsThunk";
+import { postComment, fetchComments } from "../thunks/commentsThunk";
 
 const initialState = {
   data: [],
@@ -21,8 +21,41 @@ const commentsSlice = createSlice({
       })
       .addCase(fetchComments.rejected, (state) => {
         state.status = "failed";
+      })
+      .addCase(postComment.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(postComment.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        console.log(action.payload);
+        if (action.payload.parent_id) {
+          state.data = state.data.map((comment) => {
+            if (comment.id === action.payload.parent_id) {
+              return {
+                ...comment,
+                sub_comments: [...comment.sub_comments, action.payload],
+              };
+            }
+            return comment;
+          });
+        } else {
+          state.data.push({
+            id: action.payload.id,
+            text: action.payload.text,
+            task_id: action.payload.task_id,
+            parent_id: action.payload.parent_id,
+            author_avatar: action.payload.author_avatar,
+            author_nickname: action.payload.author_nickname,
+            sub_comments: [],
+          });
+        }
+      })
+      .addCase(postComment.rejected, (state) => {
+        state.status = "failed";
       });
   },
 });
+
+// export const { } = commentsSlice.actions;
 
 export default commentsSlice.reducer;
--- src/redux/thunks/commentsThunk.js
+++ src/redux/thunks/commentsThunk.js
@@ -1,10 +1,26 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { get_comments } from "../../api/comments";
+import {get_comments, post_comment} from '../../api/comments'
+ 
+
+export const postComment = createAsyncThunk(
+  "comments/postComment",
+  async ({ data }, { rejectWithValue }) => {
+    try {
+      const response = await post_comment({
+        ...data,
+      });
+
+      return response;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
 
 export const fetchComments = createAsyncThunk(
-  "departments/fetchDepartments",
-  async ({task_id}) => {
-    const response = await get_comments({task_id});
+  "comments/fetchComments",
+  async ({ taskId }) => {
+    const response = await get_comments({ taskId: taskId });
     return response;
   }
 );
